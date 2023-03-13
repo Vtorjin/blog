d28260df4e6f21ba9c721a789f7908111af6af88
Vtorjin <480318557@qq.com>
2023-03-13 11:45:25 +0800

    add

---
+++ assets/js/28.3e09139b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{892:function(n,t,s){"use strict";s.r(t);var e=s(1),a=Object(e.a)({},(function(){var n=this._self._c;return n("div",{staticClass:"content"},[n("p",[n("beiAn",{attrs:{home:!0}})],1)])}),[],!1,null,null,null);t.default=a.exports}}]);
